5dad01659613400627bad601875cdbda694d02c9
Mridul Tiwari <85994338+mridult543@users.noreply.github.com>
2021-08-03 10:05:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Review_1 from './Review_1'
 import Carousel from './Carousel'
+import Image from './Image'
 function App() {
   return (
     <div className="App">
       <Review_1 />
       <Carousel />
+      <Image />
     </div>
   )
 }
